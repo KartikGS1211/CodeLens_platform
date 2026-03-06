cac28703a3b3c8dbc192fd7ac1d6fbb2befb91c9
KartikGS1211 <kartiksarode05@gmail.com>
2026-03-06 13:18:55 +0530

    made changes in app js file and env

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,14 @@ import analysisRoutes from "./routes/analysisroutes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+    {
+    origin: [
+      "http://localhost:5173",
+      "https://codelens-ai-752d.onrender.com"
+    ],
+  }
+));
 app.use(express.json());
 
 app.use("/api/analysis", analysisRoutes);
